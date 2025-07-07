eaecd2a20f3deb2f86ab633a5e60171041eb97a7
hacxy <hacxy.js@outlook.com>
2025-07-07 16:47:47 +0800

    ci: change test ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    outputFile: './test-report/index.html',
-    reporters: ['default', 'html'],
+    // outputFile: './test-report/index.html',
+    // reporters: ['default', 'html'],
     coverage: {
       enabled: true,
       include: ['src'],
       reporter: ['text-summary', 'html'],
-      reportsDirectory: './test-report/coverage'
+      // reportsDirectory: './coverage'
     },
   }
 });
